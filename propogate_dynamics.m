a4c644bada78a8a0fbe320e2c78aec5d6df86b5f
Chelsea Sidrane <csidrane@stanford.edu>
2017-05-28 17:30:05 -0700

    added unicycle dynamics model

--- propogate_dynamics.m
+++ propogate_dynamics.m
@@ -1,5 +1,10 @@
 % dynamics function
-
-function [next_state] = propogate_dynamics(stuff)
+% unicycle robot model
+% state is [x_position, y_position, orientation]'
+function [next_state] = propogate_dynamics(prev_state, velocity, rotation_rate, dt)
+    theta_t = prev_state(3);
+    next_state = prev_state + dt*[velocity*cos(theta_t);...
+                                  velocity*sin(theta_t);...
+                                  rotation_rate];
 
 end
