e4c5089c9faa53e98ec03526f9623841ee4b48e0
Chelsea Sidrane <csidrane@stanford.edu>
2017-05-28 18:29:31 -0700

    added initialization stuff to main loop and also updated dynamics with an option to add noise

--- main_loop.m
+++ main_loop.m
@@ -1,26 +1,50 @@
 % a final project
 % brought to you in three acts
 
+%%%%%%%%%%%%%%%%%%%%% Current Specifications %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% state: [x_position, y_position, theta]
+% control: [velocity, rotation_rate]
+% filter used: UKF
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % init sim parameters
+t_max = 20;
+dt = 0.001;
+time = 0:dt:t_max;
 
 % init containers
+state_dim = 3; 
+x = zeros(3,length(time)); % the state
+mu = zeros(3,length(time)); % the estimated state
+sigma = zeros(3,3,length(time)); % the covariance
+
+% initial filter state + real state
+x(:,1) = [0,0,0];
+mu(:,1) = [0,0,0];
+sigma(:,:,1) = 1e6*ones(3,3); % very uncertain start state
+
+% noise parameters
+meas_noise_cov = 0.01; % R
+process_noise_cov = 0.01*diag(ones(3,1));
 
 % main simulation loop
 
 % for each timestep 
+for t = time(1:end-1)
 
     % propogate dynamics
-    [next_state] = propogate_dynamics(prev_state, velocity, rotation_rate, dt);
+    add_proc_noise = true;
+    [next_state] = propogate_dynamics(prev_state, velocity, rotation_rate, dt, process_noise_cov, add_proc_noise);
     
     % measure + estimate state
-    add_noise = true;
-    measurement = get_measurement(state, meas_noise_cov, add_noise);
+    add_meas_noise = true;
+    measurement = get_measurement(x(:,t), meas_noise_cov, add_meas_noise);
     [state, cov] = get_estimate(x, y, z);
     
     % do active control
     [control] = get_control(a,b,c);
 
-    
 % end for
+end
 
 % plot stuff (call plot stuff function)
--- propogate_dynamics.m
+++ propogate_dynamics.m
@@ -1,10 +1,13 @@
 % dynamics function
 % unicycle robot model
 % state is [x_position, y_position, orientation]'
-function [next_state] = propogate_dynamics(prev_state, velocity, rotation_rate, dt)
+function [next_state] = propogate_dynamics(prev_state, velocity, rotation_rate, dt, noise_cov, add_noise)
     theta_t = prev_state(3);
     next_state = prev_state + dt*[velocity*cos(theta_t);...
                                   velocity*sin(theta_t);...
                                   rotation_rate];
-
+    if add_noise
+        wt = mvnrnd([0;0;0],noise_cov);
+        next_state = next_state + wt;
+    end                         
 end
