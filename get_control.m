027541ca7b9e0b797017c72621c5a6cd429e1da6
nivetaiyer <nivetaiyer@gmail.com>
2017-05-28 18:58:58 -0700

    Updated both functions

--- get_control.m
+++ get_control.m
@@ -1,5 +1,17 @@
 % active control piece
 
-function [control] = get_control(a,b,c)
+function [u_best] = get_control(@objective_function,u0, finite_horizon)
+objective = objective_function(x,sig,y,w);
+%One step horizon
+u_best = fminunc(objective, u0);
 
+%10 step horizon
+if finite_horizon
+    for i = 1:10
+    %propogate the model, get the objective, %propogate the model, get the
+    %objective (i times)
+    %keep adding the objective
+    u_best = fminunc(objective,u0);
+    end
+end
 end
--- objective_function.m
+++ objective_function.m
@@ -1,1 +1,7 @@
+function [objective] = objective_function(x,sig,y,w)
+[mu, sigma] = get_estimate(x,sig,y,w);
+objective = log(det(sigma));
+end
+
+
 
